b30473c1863252b71b96cfd0e45a0dbdc832a80e
Sammy Chang <sammychang2185@gmail.com>
2021-11-21 19:32:23 -0800

    Separate solution and problem files

---
+++ solutions/00-template/solution.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+// const path = require('path');
+const { performance } = require('perf_hooks');
+const { URL } = require('url');
+const startTime = performance.now();
+const solution = runSolution();
+const endTime = performance.now();
+console.log(`Solution: ${solution}`);
+console.log(`Solution took ${endTime - startTime} ms.`);
+function getInput(file) {
+    return fs.readFileSync(file, 'utf8');
+}
+function parseInput(data) {
+    return data.split('\r\n');
+}
+function runSolution() {
+    const file = `${__dirname.replace('\\solutions\\', '\\problems\\')}\\input.txt`;
+    const rawInputData = getInput(file);
+    const parsedInputData = parseInput(rawInputData);
+    console.log(parsedInputData);
+}
+//# sourceMappingURL=solution.js.map
--- solutions/00-template/solution.ts
+++ solutions/00-template/solution.ts
@@ -2,21 +2,24 @@ const fs = require('fs');
 // const path = require('path');
 const { performance } = require('perf_hooks');
 const { URL } = require('url');
+
 const startTime = performance.now();
 const solution = runSolution();
 const endTime = performance.now();
 console.log(`Solution: ${solution}`);
 console.log(`Solution took ${endTime - startTime} ms.`);
-function getInput(file) {
-    return fs.readFileSync(file, 'utf8');
+
+function getInput(file: string) {
+  return fs.readFileSync(file, 'utf8');
 }
-function parseInput(data) {
-    return data.split('\r\n');
+
+function parseInput(data: string) {
+  return data.split('\r\n');
 }
+
 function runSolution() {
-    const file = `${__dirname}/input.txt`;
-    const rawInputData = getInput(file);
-    const parsedInputData = parseInput(rawInputData);
-    console.log(parsedInputData);
+  const file = `${__dirname.replace('\\solutions\\', '\\problems\\')}\\input.txt`;
+  const rawInputData = getInput(file);
+  const parsedInputData = parseInput(rawInputData);
+  console.log(parsedInputData);
 }
-//# sourceMappingURL=solution.js.map
